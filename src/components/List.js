8ffd1c3a12131db3cb61c44c0f093b9da7f3119d
harisli <harisli@tencent.com>
2022-05-09 15:16:21 +0800

    first commit

---
+++ src/components/List.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { list } from '../data/data';
+import { Button, Image, Modal, Space, Toast, Divider } from 'antd-mobile';
+import './List.css';
+
+const Example = function() {
+  return (
+    <>
+      <div className='list'>
+        {list.map(v =>
+          <div className='list-item' key={v.name}>
+            <div className='item-con'>
+              <Image src={v.imgsrc} fit='fill' />
+              <p className='item-name'>{v.name}</p>
+              <Button
+                size='small'
+                color='primary'
+                className='btn'
+                onClick={() => {
+                  Modal.alert({
+                    image: v.imgsrc,
+                    title: '学习',
+                    content: '学习说明....',
+                  })
+                }}
+              >
+                学习
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Example;
---
+++ src/data/data.js
@@ -0,0 +1,36 @@
+export const list = [
+  {
+    name: 'test1',
+    price: '11',
+    description: 'test',
+    imgsrc: 'https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'},
+  {
+    name: 'test2',
+    price: '12',
+    description: 'test',
+    imgsrc: 'https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'},
+  {
+    name: 'test3',
+    price: '13',
+    description: 'test',
+    imgsrc: 'https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'},
+  {
+    name: 'test4',
+    price: '14',
+    description: 'test',
+    imgsrc: 'https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'},
+  {
+    name: 'test5',
+    price: '15',
+    description: 'test',
+    imgsrc: 'https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'},
+  {
+    name: 'test6',
+    price: '16',
+    description: 'test',
+    imgsrc: 'https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'},
+  {
+    name: 'test7',
+    price: '17',
+    description: 'test', imgsrc: 'https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60'},
+]
